e68e1bbff50b08eba70d6af10e7a7d71e8567ad4
eli310 <Elihillman@hotmail.com>
2023-06-30 15:08:15 +1000

    Rename GS API to CS

--- GoldSysKernel/Kernel.cs
+++ GoldSysKernel/Kernel.cs
@@ -1,6 +1,6 @@
 ﻿using Cosmos.Core.Memory;
 using GoldSysKernel.Core;
-using GoldSysKernel.Core.GS;
+using GoldSysKernel.Core.CS;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,10 +17,10 @@ namespace GoldSysKernel
             bootstage = 0;
             Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
             Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
-            GSLog.LogTerminal = 0;
-            GSLog.Log("Kernel.cs","File system initialised!",GSLogType.Ok);
+            CSLog.LogTerminal = 0;
+            CSLog.Log("Kernel.cs","File system initialised!",GSLogType.Ok);
             Console.ForegroundColor = ConsoleColor.Yellow;
-            GSTerminal.WriteLine("GoldSys - Milestone 1 (0.1)", 0);
+            CSTerminal.WriteLine("GoldSys - Milestone 1 (0.1)", 0);
             Console.ForegroundColor = ConsoleColor.White;
             bootstage = -1;
         }
@@ -29,9 +29,9 @@ namespace GoldSysKernel
         {
             try
             {
-                if (GSTerminal.DisplayTerminal == 0)
+                if (CSTerminal.DisplayTerminal == 0)
                 {
-                    GSTerminal.Write(Shell.GetFullPath()+"> ", 0);
+                    CSTerminal.Write(Shell.GetFullPath()+"> ", 0);
                     string cmd = Console.ReadLine();
                     try
                     {
@@ -50,14 +50,14 @@ namespace GoldSysKernel
         }
         private static void Crash(Exception e)
         {
-            GSTerminal.DisplayTerminal = -1;
+            CSTerminal.DisplayTerminal = -1;
             Console.BackgroundColor = ConsoleColor.Red;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
-            GSTerminal.WriteLine("Oops! GoldSys has crashed.", -1);
-            GSTerminal.WriteLine("The exception was: "+e.ToString(), -1);
-            GSTerminal.WriteLine("Please contact the developer, eli310#9755!", -1);
-            GSTerminal.WriteLine("You need to manually restart", -1);
+            CSTerminal.WriteLine("Oops! GoldSys has crashed.", -1);
+            CSTerminal.WriteLine("The exception was: "+e.ToString(), -1);
+            CSTerminal.WriteLine("Please contact the developer, eli310#9755!", -1);
+            CSTerminal.WriteLine("You need to manually restart", -1);
         }
     }
 }

60fb6f77212c097b181aaa7dde1fcbe96967eae0
eli310 <Elihillman@hotmail.com>
2023-07-15 18:53:19 +1000

    Add DotNetParser

---
+++ GoldSysKernel/Core/Process.cs
@@ -0,0 +1,31 @@
+﻿using libDotNetClr;
+using LibDotNetParser.CILApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSysKernel.Core
+{
+    internal class Process
+    {
+        public DotNetClr clr;
+        public DotNetFile fl;
+        public Process(DotNetFile file)
+        {
+            fl = file;
+            clr = new DotNetClr(fl);
+        }
+        public Process(byte[] file)
+        {
+            fl = new DotNetFile(file);
+            clr = new DotNetClr(fl);
+        }
+        public Process(DotNetFile file, DotNetClr dclr)
+        {
+            fl = file;
+            clr = dclr;
+        }
+    }
+}

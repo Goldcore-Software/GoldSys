58a0101d01e744f10db342c41c8e49e7407654da
eli310 <Elihillman@hotmail.com>
2023-08-25 00:47:39 +1000

    Add this back here

---
+++ GoldSysKernel/GSystem/TestGUIApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSysKernel.GSystem
+{
+    internal class TestGUIApp : Window
+    {
+        public override void Draw()
+        {
+            
+        }
+
+        public override void Open()
+        {
+            Title = "Test Application";
+        }
+    }
+}
---
+++ GoldSysKernel/GSystem/Window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSysKernel.GSystem
+{
+    public abstract class Window
+    {
+        public string Title = "Window";
+        public int SizeX = 640;
+        public int SizeY = 420;
+        public int PositionX = 0;
+        public int PositionY = 60;
+        public abstract void Draw();
+        public abstract void Open();
+        public void DrawTitleBar()
+        {
+            GSManager.screen.DrawFilledRectangle(Color.Aqua,PositionX,PositionY-30,SizeX,30);
+            GSManager.screen.DrawFilledRectangle(Color.White,PositionX,PositionY,SizeX,SizeY);
+        }
+    }
+}
